27a4418573e185556b410d97609983b9c95a96ca
mzahed92 <mmzahed992@gmail.com>
2019-10-06 20:59:55 -0700

    Added libraries and created pwd method.

--- Project 1/proj1.c
+++ Project 1/proj1.c
@@ -1,8 +1,24 @@
-#include <stdio.h>
+/*                                      **
+** COMP 322/L Project 1                 **
+**                                      **
+** This little project simulates some   **
+** Linux commands in C.                 **
+**				  - Mohammad M. Zahed   **
+*/
 
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+
+
+// Method to print the current working directory.
+void pwd(){
 	
-	printf("Hello, world!\n");
+	char *temp;
 	
-	return 0;
+	temp = ( char * )malloc( 100 * sizeof( char ) );
+	getcwd( temp,100 );
+	
+	printf( "\nThe current working directory is: %s \n", temp );
 }
